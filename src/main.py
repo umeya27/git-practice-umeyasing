a23c4187449ecd348891613704d480d1674728b4
umeya27 <umeyasing.gb30@gmail.com>
2026-09-17 12:31:50 +0600

    Add divition calculator function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, multiply, subtract
+from utils import add, multiply, subtract, division
 
 print("name : umeya sing.")
 print("Today's date:", date.today())
@@ -7,3 +7,4 @@ print("Today's date:", date.today())
 print("Addition: ", add(5, 3))
 print("Subtraction: ", subtract(10, 4))
 print("Multiplication: ", multiply(6, 7))
+print("divition: ", division(8,2))
--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,5 @@ def subtract(a, b):
     return a - b
 def multiply(a , b):
     return a * b
+def division(a, b):
+    return a/b
